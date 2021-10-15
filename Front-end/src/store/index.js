1a52cfbc04349c1d5ad684f6f382ea2e9f2bf3bf
Mario Escalante Cantu <a16021553@iteshu.edu.mx>
2021-10-15 14:36:34 -0500

    Modificacion para agregar el modulo LoginStore

--- Front-end/src/store/index.js
+++ Front-end/src/store/index.js
@@ -1,17 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import RegistroStore from "../components/Registro/RegistroStore";
+import LoginStore from '../components/Login/LoginStore'
 import SnackStore from '../components/Snackbar/SnackStore'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(Vuex)
-axios.defaults.baseURL = "http://localhost:5001/api/";
+axios.defaults.baseURL = "http://localhost:5000/api/";
 Vue.use(VueAxios, axios)
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: null,
+  },
   mutations: {
+    setUserProfile(state, user) {
+      state.user = user
+      console.log(user)
+    }
   },
   actions: {
   },
-  modules: { RegistroStore, SnackStore },
+  modules: { RegistroStore, SnackStore, LoginStore },
 });
