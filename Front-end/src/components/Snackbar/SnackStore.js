765e4cc58e65a7b6d9a8a9930436f66102dee851
Mario Escalante Cantu <a16021553@iteshu.edu.mx>
2021-10-15 14:34:45 -0500

    Creacion de snack para mensajes

--- Front-end/src/components/Snackbar/SnackStore.js
+++ Front-end/src/components/Snackbar/SnackStore.js
@@ -2,15 +2,12 @@ const store = {
     namespaced: true,
     state: { viewSnack: false, textSnack: 'Im a snackbar' },
     mutations: {
-        SetSnack(state, properties) {
-            state.viewSnack = properties.show
-            state.textSnack = properties.text
+        SetSnack(state, text) {
+            state.viewSnack = true
+            state.textSnack = text
         }
     },
     actions: {
-        showHidenSnack({ commit }, properties) {
-            commit('SetSnack', properties)
-        },
         setShow({ state }, show) {
             state.viewSnack = !show
         }
---
+++ Front-end/src/components/sliderBar/SliderBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-navigation-drawer permanent expand-on-hover dark app>
+    <v-list>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">Mario Escalante</v-list-item-title>
+          <v-list-item-subtitle>
+            616868d3b7f15db2aa28a48f
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: "SliderBar",
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "dashboard" },
+        { title: "Photos", icon: "image" },
+        { title: "About", icon: "help" },
+      ],
+    };
+  },
+};
+</script>
