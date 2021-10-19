a16762ae0022accd776bc3088c307087d4ffa333
Mario Escalante Cantu <a16021553@iteshu.edu.mx>
2021-10-19 18:48:13 -0500

    Creacion del filter formatDate

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -8,6 +8,15 @@ import VueAxios from 'vue-axios'
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 Vue.use(VueAxios, axios)
+
+import moment from 'moment';
+
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('MM-DD-YYYY hh:mm')
+  }
+});
+
 Vue.config.productionTip = false;
 new Vue({
   router,
